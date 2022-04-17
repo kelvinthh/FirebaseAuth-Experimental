5f2a4ccc4484e5d7ab5614bafc3f730dd2eb1ffc
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2022-04-17 09:16:07 +0800

    Native Notify

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import registerNNPushToken from 'native-notify';
+
 import LoginScreen from './screens/LoginScreen'
 import HomeScreen from './screens/HomeScreen'
+import { NN_APP_ID, NN_APP_TOKEN } from './config';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+  registerNNPushToken(NN_APP_ID, NN_APP_TOKEN);
   return (
     <NavigationContainer>
       <Stack.Navigator>
